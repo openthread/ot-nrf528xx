e18f357f9b9603eeb99e6a50ec5e5d425890351e
Zhanglong Xia <zhanglongxia@google.com>
2025-01-03 09:09:49 +0000

    [radio] correct the `mInfo.mRxInfo.mTimestamp` value of recevied frames
    The `mInfo.mRxInfo.mTimestamp` represents the end time of the SFD of the
    received frame. But the radio driver sets the `mInfo.mRxInfo.mTimestamp`
    value to the first symbol time of the received frame.
    
    This commit adds a method `nrf_802154_timestamp_end_to_phr_convert()`
    to calculate the end time of the SFD of the received frame. The method
    comes from the `sdk-nrfxlib`.

--- src/src/radio.c
+++ src/src/radio.c
@@ -1037,9 +1037,7 @@ void nrf_802154_received_timestamp_raw(uint8_t *p_data, int8_t power, uint8_t lq
 #if !NRF_802154_TX_STARTED_NOTIFY_ENABLED
 #error "NRF_802154_TX_STARTED_NOTIFY_ENABLED is required!"
 #endif
-    uint32_t offset =
-        (int32_t)otPlatAlarmMicroGetNow() - (int32_t)nrf_802154_first_symbol_timestamp_get(time, p_data[0]);
-    receivedFrame->mInfo.mRxInfo.mTimestamp = nrf5AlarmGetCurrentTime() - offset;
+    receivedFrame->mInfo.mRxInfo.mTimestamp = nrf_802154_timestamp_end_to_phr_convert(time, p_data[0]);
 
     sAckedWithFramePending = false;
 
@@ -1172,10 +1170,7 @@ void nrf_802154_transmitted_timestamp_raw(const uint8_t *aFrame,
     }
     else
     {
-        uint32_t offset =
-            (int32_t)otPlatAlarmMicroGetNow() - (int32_t)nrf_802154_first_symbol_timestamp_get(ack_time, aAckPsdu[0]);
-
-        sAckFrame.mInfo.mRxInfo.mTimestamp = nrf5AlarmGetCurrentTime() - offset;
+        sAckFrame.mInfo.mRxInfo.mTimestamp = nrf_802154_timestamp_end_to_phr_convert(ack_time, aAckPsdu[0]);
         sAckFrame.mPsdu                    = &aAckPsdu[1];
         sAckFrame.mLength                  = aAckPsdu[0];
         sAckFrame.mInfo.mRxInfo.mRssi      = aPower;
